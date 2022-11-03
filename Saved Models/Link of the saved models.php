5dab59f466bf6e615083454f2c4b61e71dcf7e9f
Franch Lee Bataan <117342232+FranchLeeBataan@users.noreply.github.com>
2022-11-03 19:06:17 +0800

    Update and rename index.php to Link of the saved models.php

---
+++ Saved Models/Link of the saved models.php
@@ -0,0 +1,3 @@
+Below is the link of the saved models. The model is run at 500,000 timesteps where models are saved at every 20,0000 timesteps. 
+The gdrive link contains 25 models in a .zip file format
+https://drive.google.com/drive/folders/1lJklPhJ3yP133ln5Vw0-kbQv3wR97Z7-?usp=sharing 
